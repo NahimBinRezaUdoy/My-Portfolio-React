e89588b735fa4e7936dc10a28f5ac775f47eae00
Nahim Udoy <67038120+NahimBinRezaUdoy@users.noreply.github.com>
2022-03-02 17:49:52 +0600

    Creating Working Environment

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './contact.css'
+const contact = () => {
+  return (
+    <div>contact</div>
+  )
+}
+
+export default contact
---
+++ src/components/testimonial/Testmonial.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './testmonial.css'
+
+const Testmonial = () => {
+  return (
+    <div>Testmonial</div>
+  )
+}
+
+export default Testmonial
