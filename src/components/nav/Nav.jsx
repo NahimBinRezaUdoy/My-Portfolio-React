3059d86a1de3e19d2a97a083b4fca49a28364fc8
Nahim Udoy <67038120+NahimBinRezaUdoy@users.noreply.github.com>
2022-03-03 15:36:37 +0600

    Added Nav markup

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -6,11 +6,15 @@ import {BsBookmarkStar} from 'react-icons/bs'
 import {RiServiceLine} from 'react-icons/ri'
 import {BiMessageSquareDetail} from 'react-icons/bi'
 const Nav = () => {
-  
-    return (
-      <section id='nav'>Nav</section>
-    )
-  }
-
+  return (
+    <nav>
+      <a href="#"><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BsBookmarkStar/></a>
+      <a href="#services"><RiServiceLine/></a>
+      <a href="#contact"><BiMessageSquareDetail/></a>
+    </nav>
+  )
+}
 
 export default Nav
