77ab0895e0c558aa854d606d796a854747836403
Nahim Udoy <67038120+NahimBinRezaUdoy@users.noreply.github.com>
2022-03-06 00:10:27 +0600

    Use map method for getting portfolio items from array

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -12,6 +12,72 @@ import PostShare from '../../assets/weshare.PNG'
 import Survey from '../../assets/survey.PNG'
 
 
+const data = [
+  {
+    id: 1,
+    image: MovieDesign,
+    title: 'Movie Website',
+    github: 'https://github.com/NahimBinRezaUdoy/MovieWebsiteDesign',
+    demo: 'https://udmovie.netlify.app/'
+  },
+  {
+    id: 2,
+    image: PortfolioWeb,
+    title: 'This is My Portfolio',
+    github: 'https://github.com/NahimBinRezaUdoy/MyResume',
+    demo: 'https://nahimudoy.netlify.app/',
+  },
+  {
+    id: 3,
+    image: Resume,
+    title: 'This is My Resume',
+    github: 'https://github.com/NahimBinRezaUdoy/ResumeDesign',
+    demo: 'https://nahimresume.netlify.app/',
+  },
+  {
+    id: 4,
+    image: DarkClub,
+    title: 'DarkClub Website (Design)',
+    github: 'https://github.com/NahimBinRezaUdoy/DarkClub',
+    demo: 'https://darkclub.netlify.app/',
+  },
+  {
+    id: 5,
+    image: DonationNation,
+    title: 'Socail Website(Laravel)',
+    github: 'https://github.com/NahimBinRezaUdoy/DonationNation',
+    demo: 'https://www.youtube.com/watch?v=H7gUID97xOo',
+  },
+  {
+    id: 6,
+    image: DigitalClassroom,
+    title: 'Online Education System',
+    github: 'https://github.com/NahimBinRezaUdoy/Education-Server',
+    demo: '#',
+  },
+  {
+    id: 7,
+    image: Movie,
+    title: 'Movie Website (Laravel)',
+    github: 'https://github.com/NahimBinRezaUdoy/MovieApp',
+    demo: 'https://www.youtube.com/watch?v=MYBY-AKorJw',
+  },
+  {
+    id: 8,
+    image: PostShare,
+    title: 'Post Shareing App (Laravel)',
+    github: 'https://github.com/NahimBinRezaUdoy/WeShare',
+    demo: 'https://www.youtube.com/watch?v=NTmgIBxfSAs',
+  },
+  {
+    id: 9,
+    image: Survey,
+    title: 'Survey Website (Laravel)',
+    github: 'https://github.com/NahimBinRezaUdoy/WeShare',
+    demo: 'https://www.youtube.com/watch?v=NTmgIBxfSAs',
+  },
+]
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
@@ -19,113 +85,22 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={MovieDesign} alt="" />
-          </div>
-            <h3>Movie Website</h3>
-            <div className="portfolio__item-cta">
-              <a href="https://github.com/NahimBinRezaUdoy/MovieWebsiteDesign" className='btn' target="_blank" >Github</a>
-              <a href="https://udmovie.netlify.app/" className='btn btn-primary' target="_blank" >Live Demo</a>  
-            </div>
-            
-        </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={PortfolioWeb} alt="" />
-          </div>
-            <h3>This is My Portfolio</h3>
-            <div className="portfolio__item-cta">
-              <a href="https://github.com/NahimBinRezaUdoy/MyResume" className='btn' target="_blank" >Github</a>
-              <a href="https://nahimudoy.netlify.app/" className='btn btn-primary' target="_blank" >Live Demo</a>  
-            </div>
-            
-        </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={Resume} alt="" />
-          </div>
-            <h3>This is My Resume</h3>
-            <div className="portfolio__item-cta">
-              <a href="https://github.com/NahimBinRezaUdoy/ResumeDesign" className='btn' target="_blank" >Github</a>
-              <a href="https://nahimresume.netlify.app/" className='btn btn-primary' target="_blank" >Live Demo</a>  
-            </div>
-            
-        </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={DarkClub} alt="" />
-          </div>
-            <h3>DarkClub Website (Design)</h3>
-            <div className="portfolio__item-cta">
-              <a href="https://github.com/NahimBinRezaUdoy/DarkClub" className='btn' target="_blank" >Github</a>
-              <a href="https://darkclub.netlify.app/" className='btn btn-primary' target="_blank" >Live Demo</a>  
-            </div>
-            
-        </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={DonationNation} alt="" />
-          </div>
-            <h3>Socail Website(Laravel)</h3>
-            <div className="portfolio__item-cta">
-              <a href="https://github.com/NahimBinRezaUdoy/DonationNation" className='btn' target="_blank" >Github</a>
-              <a href="https://www.youtube.com/watch?v=H7gUID97xOo" className='btn btn-primary' target="_blank" >Live Demo</a>  
-            </div>
-            
-        </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={DigitalClassroom} alt="" />
-          </div>
-            <h3>Online Education System</h3>
-            <div className="portfolio__item-cta">
-              <a href="https://github.com/NahimBinRezaUdoy/Education-Server" className='btn' target="_blank" >Github</a>
-              <a href="#" className='btn btn-primary' target="_blank" >Live Demo</a>  
-            </div>
-            
-        </article>
-        
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={Movie} alt="" />
-          </div>
-            <h3>Movie Website (Laravel)</h3>
-            <div className="portfolio__item-cta">
-              <a href="https://github.com/NahimBinRezaUdoy/MovieApp" className='btn' target="_blank" >Github</a>
-              <a href="https://www.youtube.com/watch?v=MYBY-AKorJw" className='btn btn-primary' target="_blank" >Live Demo</a>  
-            </div>
-            
-        </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={PostShare} alt="" />
-          </div>
-            <h3>Post Shareing App (Laravel)</h3>
-            <div className="portfolio__item-cta">
-              <a href="https://github.com/NahimBinRezaUdoy/WeShare" className='btn' target="_blank" >Github</a>
-              <a href="https://www.youtube.com/watch?v=NTmgIBxfSAs" className='btn btn-primary' target="_blank" >Live Demo</a>  
-            </div>
-            
-        </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={Survey} alt="" />
-          </div>
-            <h3>Survey Website (Laravel)</h3>
-            <div className="portfolio__item-cta">
-              <a href="https://github.com/NahimBinRezaUdoy/WeShare" className='btn' target="_blank" >Github</a>
-              <a href="https://www.youtube.com/watch?v=NTmgIBxfSAs" className='btn btn-primary' target="_blank" >Live Demo</a>  
-            </div>
-            
-        </article>
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+            <article key={id} className='portfolio__item'>
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className='btn' target="_blank" >Github</a>
+                <a href={demo} className='btn btn-primary' target="_blank" >Live Demo</a>  
+              </div>
+            </article>
+            )
+          })
+        }
       </div>
     </section>
   )
