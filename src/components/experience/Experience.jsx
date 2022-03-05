86b1b7cdeb5777d5b90e2f49cce9f09d6e07dc2c
Nahim Udoy <67038120+NahimBinRezaUdoy@users.noreply.github.com>
2022-03-05 21:14:09 +0600

    Added Experience Styling

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -14,33 +14,51 @@ const Experience = () => {
           <div className="experience__content">
             <article className='experience__details'>
               <BsBookmarkCheckFill className='experience__details__icon'/>
-              <h4>HTML</h4>
-              <small className='text-light'>Experienced</small>
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+              
             </article>
             <article className='experience__details'>
               <BsBookmarkCheckFill className='experience__details__icon'/>
-              <h4>CSS</h4>
-              <small className='text-light'>Experienced</small>
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+              
             </article>
             <article className='experience__details'>
               <BsBookmarkCheckFill className='experience__details__icon'/>
-              <h4>JavaScript</h4>
-              <small className='text-light'>Intermediate</small>
+              <div>
+                <h4>JavaScript</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+              
             </article>
             <article className='experience__details'>
               <BsBookmarkCheckFill className='experience__details__icon'/>
-              <h4>Bootstrap</h4>
-              <small className='text-light'>Experienced</small>
+              <div>
+                <h4>Bootstrap</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+              
             </article>
             <article className='experience__details'>
               <BsBookmarkCheckFill className='experience__details__icon'/>
-              <h4>Tailwind</h4>
-              <small className='text-light'>Intermediate</small>
+              <div>
+                <h4>Tailwind</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+              
             </article>
             <article className='experience__details'>
               <BsBookmarkCheckFill className='experience__details__icon'/>
-              <h4>React Js</h4>
-              <small className='text-light'>Intermediate</small>
+              <div>
+                <h4>React Js</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+              
             </article>
           </div>
         </div>
@@ -52,33 +70,51 @@ const Experience = () => {
           <div className="experience__content">
             <article className='experience__details'>
               <BsBookmarkCheckFill className='experience__details__icon'/>
-              <h4>PHP</h4>
-              <small className='text-light'>Intermediate</small>
+              <div>
+                <h4>PHP</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+              
             </article>
             <article className='experience__details'>
               <BsBookmarkCheckFill className='experience__details__icon'/>
-              <h4>Laravel</h4>
-              <small className='text-light'>Experienced</small>
+              <div>
+                <h4>Laravel</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+              
             </article>
             <article className='experience__details'>
               <BsBookmarkCheckFill className='experience__details__icon'/>
-              <h4>MySQL</h4>
-              <small className='text-light'>Intermediate</small>
+              <div>
+                <h4>MySQL</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+              
             </article>
             <article className='experience__details'>
               <BsBookmarkCheckFill className='experience__details__icon'/>
-              <h4>Python</h4>
-              <small className='text-light'>Basic</small>
+              <div>
+                <h4>Python</h4>
+                <small className='text-light'>Basic</small>
+              </div>
+              
             </article>
             <article className='experience__details'>
               <BsBookmarkCheckFill className='experience__details__icon'/>
-              <h4>C++</h4>
-              <small className='text-light'>Intermediate</small>
+              <div>
+                <h4>C++</h4>
+                <small className='text-light'>Basic</small>
+              </div>
+              
             </article>
             <article className='experience__details'>
               <BsBookmarkCheckFill className='experience__details__icon'/>
-              <h4>Java</h4>
-              <small className='text-light'>Basic</small>
+              <div>
+                <h4>Java</h4>
+                <small className='text-light'>Basic</small>
+              </div>
+              
             </article>
           </div>
         </div>
