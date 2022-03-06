d757f4531a9fec1fd424e8099269d93fb1928c1b
Nahim Udoy <67038120+NahimBinRezaUdoy@users.noreply.github.com>
2022-03-06 13:06:56 +0600

    Createing Slider using SwiperJs

--- src/components/testimonial/Testmonial.jsx
+++ src/components/testimonial/Testmonial.jsx
@@ -5,6 +5,16 @@ import Lemon from '../../assets/lemon.jpg'
 import Nayeem from '../../assets/nayeem.jpg'
 import Evan from '../../assets/evan.jpg'
 
+// Import Swiper React components
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 const data = [
   {
     avatar: Tasin,
@@ -34,11 +44,15 @@ const Testmonial = () => {
       <h5>Review from clients</h5>
       <h2>Testmonials</h2>
 
-      <div className="container testimonial__container">
+      <Swiper className="container testimonial__container"
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}>
         {
           data.map(({avatar, name, review}, index) => {
             return (
-              <article key={index} className="testimonial">
+              <SwiperSlide key={index} className="testimonial">
               <div className="client__avatar">
                 <img src={avatar} alt="Avatar One" />
               </div>
@@ -46,11 +60,11 @@ const Testmonial = () => {
               <small className='client__review'>
                 {review}
               </small>
-            </article>
+            </SwiperSlide>
             )
           })
         }
-      </div>
+      </Swiper>
     </section>
   )
 }
