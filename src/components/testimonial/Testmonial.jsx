860d340f141c3afcfce6a32554eda8b70b09a4ba
Nahim Udoy <67038120+NahimBinRezaUdoy@users.noreply.github.com>
2022-03-06 10:55:59 +0600

    Added Testimonial Markup

--- src/components/testimonial/Testmonial.jsx
+++ src/components/testimonial/Testmonial.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
 import './testmonial.css'
+import Tasin from '../../assets/tasin.jpg'
+import Lemon from '../../assets/lemon.jpg'
+import Nayeem from '../../assets/nayeem.jpg'
+import Evan from '../../assets/evan.jpg'
 
 const Testmonial = () => {
   return (
-    <section id='testnmonial'>Testmonial</section>
+    <section id='testnmonial'>
+      <h5>Review from clients</h5>
+      <h2>Testmonials</h2>
+
+      <div className="container testimonial__container">
+
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={Tasin} alt="Avatar One" />
+            <h5 className='client__name'>Tasin Khan</h5>
+            <small className='client__review'>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi non molestiae eligendi ipsa quas maiores vitae repellendus corrupti officiis dolor!
+            </small>
+          </div>
+        </article>
+
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={Lemon} alt="Avatar One" />
+            <h5 className='client__name'>Lemon Khan</h5>
+            <small className='client__review'>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi non molestiae eligendi ipsa quas maiores vitae repellendus corrupti officiis dolor!
+            </small>
+          </div>
+        </article>
+
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={Nayeem} alt="Avatar One" />
+            <h5 className='client__name'>Even Khan</h5>
+            <small className='client__review'>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi non molestiae eligendi ipsa quas maiores vitae repellendus corrupti officiis dolor!
+            </small>
+          </div>
+        </article>
+
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={Evan} alt="Avatar One" />
+            <h5 className='client__name'>Nayeem Khan</h5>
+            <small className='client__review'>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi non molestiae eligendi ipsa quas maiores vitae repellendus corrupti officiis dolor!
+            </small>
+          </div>
+        </article>
+
+      </div>
+    </section>
   )
 }
 
